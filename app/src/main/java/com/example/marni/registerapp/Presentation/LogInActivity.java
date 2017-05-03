1f145f79b05670b2a7eb177192fa667c16e776ae
marnix van der schoot <marnix van der schoot>
2017-05-03 12:01:04 +0200

    commit

---
+++ app/src/main/java/com/example/marni/registerapp/Presentation/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.example.marni.registerapp.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.marni.registerapp.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/marni/registerapp/Presentation/OrderActivity.java
+++ app/src/main/java/com/example/marni/registerapp/Presentation/OrderActivity.java
@@ -1,13 +1,15 @@
-package com.example.marni.registerapp;
+package com.example.marni.registerapp.Presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.marni.registerapp.R;
+
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_order);
     }
 }
