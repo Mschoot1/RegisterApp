e95ad73b0cb44e5160d0f2595ac23502b1673f4c
marnix van der schoot <marnix van der schoot>
2017-05-03 12:17:07 +0200

    commit

--- app/src/main/java/com/example/marni/registerapp/Presentation/OrderActivity.java
+++ app/src/main/java/com/example/marni/registerapp/Presentation/OrderActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.marni.registerapp.R;
 
-public class LogInActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_order);
     }
 }
