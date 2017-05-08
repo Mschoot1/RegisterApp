6d44809fbb733f04f0e6e0db1469ff94d01c91e5
wilcotrimborn <wtrimbor@student.avans.nl>
2017-05-08 15:47:15 +0200

    Added errors

--- app/src/main/java/com/example/marni/registerapp/Presentation/AddItemActivity.java
+++ app/src/main/java/com/example/marni/registerapp/Presentation/AddItemActivity.java
@@ -1,25 +1,49 @@
 package com.example.marni.registerapp.Presentation;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.widget.EditText;
 
 import com.example.marni.registerapp.R;
 
 public class AddItemActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        EditText nameEditText = (EditText) findViewById(R.id.editTextEmailaddress);
-        String name = nameEditText.getText().toString();
+        EditText ed = (EditText) findViewById(R.id.name);
+        EditText ed2 = (EditText) findViewById(R.id.price);
+        EditText ed3 = (EditText) findViewById(R.id.size);
+        EditText ed4 = (EditText) findViewById(R.id.alcohol_percentage);
 
-        if(TextUtils.isEmpty(name)) {
-            nameEditText.setError("Empty field");
-            return;
+        String ed_text = ed.getText().toString().trim();
+        String ed2_text = ed2.getText().toString().trim();
+        String ed3_text = ed3.getText().toString().trim();
+        String ed4_text = ed4.getText().toString().trim();
+
+        if(ed_text.isEmpty() || ed_text.length() == 0 || ed_text.equals("") || ed_text == null)
+        {
+            ed.setError("Enter name");
         }
+
+        if(ed2_text.isEmpty() || ed2_text.length() == 0 || ed2_text.equals("") || ed2_text == null)
+        {
+            ed2.setError("Enter price");
+        }
+        if(ed3_text.isEmpty() || ed3_text.length() == 0 || ed3_text.equals("") || ed3_text == null)
+        {
+            ed3.setError("Enter size");
+        }
+
+        if(ed4_text.isEmpty() || ed4_text.length() == 0 || ed4_text.equals("") || ed4_text == null)
+        {
+            ed4.setError("Enter alcohol percentage");
+        }
+
     }
 }
--- app/src/main/java/com/example/marni/registerapp/Presentation/LogInActivity.java
+++ app/src/main/java/com/example/marni/registerapp/Presentation/LogInActivity.java
@@ -41,4 +41,3 @@ public class LogInActivity extends AppCompatActivity {
         });
     }
 }
-
