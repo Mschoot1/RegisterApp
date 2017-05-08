5ed664f9faff40fe343edb72045166fd3bcede28
wilcotrimborn <wtrimbor@student.avans.nl>
2017-05-08 15:37:42 +0200

    Added errors

--- app/src/main/java/com/example/marni/registerapp/Presentation/AddItemActivity.java
+++ app/src/main/java/com/example/marni/registerapp/Presentation/AddItemActivity.java
@@ -2,6 +2,8 @@ package com.example.marni.registerapp.Presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 import com.example.marni.registerapp.R;
 
@@ -11,5 +13,13 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        EditText nameEditText = (EditText) findViewById(R.id.editTextEmailaddress);
+        String name = nameEditText.getText().toString();
+
+        if(TextUtils.isEmpty(name)) {
+            nameEditText.setError("Empty field");
+            return;
+        }
     }
 }
--- app/src/main/java/com/example/marni/registerapp/Presentation/LogInActivity.java
+++ app/src/main/java/com/example/marni/registerapp/Presentation/LogInActivity.java
@@ -2,6 +2,10 @@ package com.example.marni.registerapp.Presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.marni.registerapp.R;
 
@@ -11,5 +15,27 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button button = (Button) findViewById(R.id.buttonSignIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View V) {
+                EditText ed = (EditText) findViewById(R.id.editTextEmailaddress);
+                EditText ed2 = (EditText) findViewById(R.id.editTextPassword);
+
+                String ed_text = ed.getText().toString().trim();
+                String ed2_text = ed2.getText().toString().trim();
+
+                if(ed_text.isEmpty() || ed_text.length() == 0 || ed_text.equals("") || ed_text == null)
+                {
+                    ed.setError("Enter ID");
+                }
+
+                if(ed2_text.isEmpty() || ed2_text.length() == 0 || ed2_text.equals("") || ed2_text == null)
+                {
+                    ed2.setError("Enter password");
+                }
+            }
+        });
     }
 }
+
