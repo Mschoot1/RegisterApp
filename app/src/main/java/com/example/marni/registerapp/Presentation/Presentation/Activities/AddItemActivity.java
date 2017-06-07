76554869a87d53e1b1051c8d35cdb71754c6580a
Marc den Uil <marcuil75@gmail.com>
2017-06-07 13:43:02 +0200

    Added toolbar

--- app/src/main/java/com/example/marni/registerapp/Presentation/Presentation/Activities/AddItemActivity.java
+++ app/src/main/java/com/example/marni/registerapp/Presentation/Presentation/Activities/AddItemActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,11 @@ public class AddItemActivity extends AppCompatActivity implements ProductPutTask
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Product Edit");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         final Product product;
         product = (Product) getIntent().getExtras().getSerializable("PRODUCT");
